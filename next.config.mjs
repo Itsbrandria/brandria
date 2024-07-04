39d92263739a003d1fa32191f15d6c70436c9148
AmrTamer23 <amrtamer23mj@gmail.com>
2024-07-04 14:40:08 +0300

    feat: add hero component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "farmui.vercel.app",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
