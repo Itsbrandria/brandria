dbc034204feb6ed490946a7db41a6d6b076d79ac
AmrTamer23 <amrtamer23mj@gmail.com>
2024-07-11 21:24:02 +0300

    feat: added colored font for hero

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { ChevronRight } from "lucide-react";
 import Image from "next/image";
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button/button";
 import { ShinyTextButton } from "../ui/button/shiny-button";
 import { useTranslations } from "next-intl";
+import Balancer from "react-wrap-balancer";
 export function Hero() {
   const [state, setState] = React.useState(false);
 
@@ -24,20 +25,19 @@ export function Hero() {
     },
     {
       name: "Sky News",
-      logo: "https://dbrandria.com/wp-content/uploads/2023/06/Brandria-Logos-11.svg"
+      logo: "https://dbrandria.com/wp-content/uploads/2023/06/Brandria-Logos-11.svg",
     },
     {
       name: "talabat",
-      logo: "https://dbrandria.com/wp-content/uploads/2023/06/Brandria-Logos-09.svg"
+      logo: "https://dbrandria.com/wp-content/uploads/2023/06/Brandria-Logos-09.svg",
     },
     {
       name: "Umraa and Hajj Ministry",
-      logo: "https://dbrandria.com/wp-content/uploads/2023/06/Brandria-Logos-01.svg"
-    }
-  ]
+      logo: "https://dbrandria.com/wp-content/uploads/2023/06/Brandria-Logos-01.svg",
+    },
+  ];
 
-  const t = useTranslations('Hero');
-
+  const t = useTranslations("Hero");
 
   return (
     <div className="relative w-full flex flex-col">
@@ -54,23 +54,29 @@ export function Hero() {
       <section className="pt-28 z-10 select-none">
         <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
           <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
-            <h1 className="text-sm text-gray-700 dark:text-gray-400 group font-geist mr-auto px-5 py-2 bg-gradient-to-tr from-zinc-300/5 via-gray-400/5 to-transparent  border-[2px] border-white/5 rounded-3xl w-fit">
+            <h1 className="text-gray-700 dark:text-gray-400 group font-geist mr-auto px-5 py-2 bg-gradient-to-tr from-zinc-300/5 via-gray-400/5 to-transparent  border-[2px] border-white/5 rounded-3xl w-fit">
               Over 300+ projects finished
               <ChevronRight className="inline w-4 h-4 ml-2 group-hover:translate-x-1 duration-300" />
             </h1>
-            <h2 className="text-4xl md:text-5xl font-geist font-normal tracking-tighter lg:text-5xl text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)] !text-neutral-900 dark:!text-neutral-100 !leading-snug">
-              {t('title')}
+            <h2 className="text-4xl md:text-5xl font-bungee font-normal tracking-tighter lg:text-6xl text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)] !text-neutral-900 dark:!text-neutral-100 leading-tight">
+              <Balancer>{t("title")}</Balancer>
             </h2>
-            <p className="dark:text-neutral-200">
-              Our Digital Agency Services In Egypt connects you to new markets, clients, and customers and help you reach new horizons.
+            <p className="dark:text-neutral-200 text-lg">
+              <Balancer>
+                Our Digital Agency Services In Egypt connects you to new
+                markets, clients, and customers and help you reach new horizons.
+              </Balancer>
             </p>
             <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-              <ShinyTextButton
-                size='lg'
+              <button className="font-bold shadow-[0_4px_14px_0_rgb(255,0,0,39%)] hover:shadow-[0_6px_20px_rgba(255,0,0,23%)] hover:bg-[rgba(255,0,0,0.9)] px-8 py-2 bg-red-600 rounded-md text-white  transition duration-200 ease-linear">
+                Start your project
+              </button>
+              <Button
+                variant="outline"
+                className="dark:text-neutral-200 font-bold"
               >
-                Start Your Project
-              </ShinyTextButton>
-
+                Our Work
+              </Button>
             </div>
           </div>
           <div className="flex-none mt-14 md:mt-0 md:max-w-xl">
@@ -88,23 +94,20 @@ export function Hero() {
             Trusted by the Top Entities
           </p>
           <div className="flex justify-center items-center flex-wrap gap-x-12 gap-y-6 mt-6">
-            {
-              TopEntities.map((entity, idx) => (
-                <div key={idx} className="flex items-center justify-center">
-                  <Image
-                    src={entity.logo}
-                    alt={entity.name}
-                    width={200}
-                    height={200}
-                    className="w-36"
-                  />
-                </div>
-              ))
-            }
+            {TopEntities.map((entity, idx) => (
+              <div key={idx} className="flex items-center justify-center">
+                <Image
+                  src={entity.logo}
+                  alt={entity.name}
+                  width={200}
+                  height={200}
+                  className="w-36"
+                />
+              </div>
+            ))}
           </div>
         </div>
       </section>
     </div>
   );
 }
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,9 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fontFamily: {
+        bungee: ["Bungee", "sans-serif"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
