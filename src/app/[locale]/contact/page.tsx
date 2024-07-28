21d1cc85a22b1c7119971e3d6d7919f8dd7a5f44
yusefabbas-A <79419424+yusefabbas-A@users.noreply.github.com>
2024-07-28 10:31:53 +0300

    Contact Page

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -10,6 +10,16 @@ export default function page() {
           <h5 className='lg:max-w-5xl mx-auto space-y-4 '>Drop us a line through the form below and we will get back to you</h5>
         </div>
         <div className='grid grid-cols-8 gap-4 mx-52'>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+          </label>
+          <input
+            id="1"
+            type="text"
+            placeholder="write"
+            value="asda"
+            
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
           <div className='p-4 col-span-4 bg-slate-600'/>
           <div className='p-4 col-span-4 bg-slate-600'/>
           <div className='p-4 col-span-8 bg-slate-600'/>
