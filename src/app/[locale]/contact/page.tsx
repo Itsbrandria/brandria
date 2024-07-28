00f4637820ba803f336c16290147c8aa2c561e54
yusefabbas-A <79419424+yusefabbas-A@users.noreply.github.com>
2024-07-28 16:43:07 +0300

    contact Page Animations

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -3,6 +3,7 @@ import { useLocale, useTranslations } from 'next-intl';
 import React from 'react'
 import Balancer from 'react-wrap-balancer';
 import Image from "next/image";
+import GradualSpacing from '@/components/magicui/gradual-spacing';
 
 export default function page() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -29,7 +30,11 @@ export default function page() {
             <div className='leading-loose flex flex-col justify-center gap-6'>
               <p className='lg:max-w-5xl mx-auto space-y-4'>{t("h")}</p>
               <div className='flex justify-center items-center gap-4'>
-                <h1 className='text-2xl font-bold ltr:tracking-wider sm:text-5xl md:text-4xl text-center'>{t("h2")} <br/> {t("h2N")}</h1>
+                <div className='flex flex-col gap-0'>
+                <h1 className='text-2xl font-bold ltr:tracking-wider sm:text-5xl md:text-4xl text-center'>{t("h2")}</h1>
+                <GradualSpacing className="font-display text-center text-2xl font-bold tracking-[-0.1em]  text-black dark:text-white sm:text-5xl md:text-4xl md:leading-[5rem]"
+                text={t("h2N")}/>
+                </div>
                 <Image
                   src="/Alien.svg"
                   alt="Blue Tree"
@@ -42,55 +47,72 @@ export default function page() {
         </BlurFade>
         <form className='grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-3 mx-4 sm:mx-52'>
           <div className='p-3 col-span-1 sm:col-span-1'>
+          <BlurFade inView delay={0.5}>
             <input
               id="1"
               type="text"
               placeholder="First Name"              
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+          </BlurFade>
           </div>
           <div className='p-3 col-span-1 sm:col-span-1'>
+          <BlurFade inView delay={0.6}>
             <input
               id="2"
               type="text"
               placeholder="Last Name"              
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+          </BlurFade>
           </div>
+          
           <div className='p-3 col-span-1 sm:col-span-2'>
+          <BlurFade inView delay={0.7}>
             <input
               id="3"
               type="email"
               placeholder="Email"              
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
+              />
+              </BlurFade>
           </div>
+          
           <div className='p-3 col-span-1 sm:col-span-1'>
+          <BlurFade inView delay={0.8}>
             <input
               id="4"
               type="tel"
               placeholder="Phone Number"              
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+          </BlurFade>
           </div>
           <div className='p-3 col-span-1 sm:col-span-1'>
+          <BlurFade inView delay={0.9}>
             <input
               id="5"
               type="tel"
               placeholder="Gender"              
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
-          </div>          
+          </BlurFade>
+          </div>
           <div className='p-3 col-span-1 sm:col-span-2'>
+          <BlurFade inView delay={0.1}>      
             <textarea name="Message" id="6" rows={4} 
               placeholder="tell us a bit about your goals, what are you hoping to achieve"              
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+          </BlurFade>   
           </div>
           <div className='p-3 col-span-1'>
+          <BlurFade inView delay={0.1}>
             <input type="submit" value={t("btn")} className='border rounded py-2 px-3 bg-white text-black cursor-pointer'/>
+          </BlurFade>
           </div>
         </form>
+        <BlurFade inView delay={0.3}>
         <div className='flex flex-col mx-4 sm:mx-40'>
           <h4>{locale === "en" ? ListText.subTitle : ListText.subTitleAR}</h4>
           <ul className='list-image-[url(/badge-check.svg)]'>
@@ -99,6 +121,7 @@ export default function page() {
             <li>{locale === "en" ? ListText.element3 : ListText.element3AR}</li>
           </ul>
         </div>
+        </BlurFade>
       </div>
     </main>
   )
