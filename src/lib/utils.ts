9f82dbc8af904803b6ba5ef8915efcec57e6b3a2
AmrTamer23 <amrtamer23mj@gmail.com>
2024-07-30 11:31:36 +0300

    fix: categories in portfolio item

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const portfolio_categories: { [key: string]: number } = {
-  Branding: 8,
-  "UI/UX": 9,
-  "Web Development": 10,
-  "Video Production": 11,
+export const portfolio_categories = {
+  8: "Branding",
+  9: "UI/UX",
+  10: "Web Development",
+  11: "Video Production",
 };
