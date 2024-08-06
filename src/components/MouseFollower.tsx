9d6b91771d4fc7f70104f250c7709cb2caaebfa2
yusefabbas-A <79419424+yusefabbas-A@users.noreply.github.com>
2024-08-06 13:50:33 +0300

    portfolio Page

--- src/components/MouseFollower.tsx
+++ src/components/MouseFollower.tsx
@@ -2,12 +2,16 @@
 
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import Image from "next/image"
+import { cn } from '@/lib/utils';
 
 interface MouseFollowerProps {
   children?: React.ReactNode;
+  text?:string;
+  className?:string;
 }
 
-const MouseFollower: React.FC<MouseFollowerProps> = ({ children }) => {
+const MouseFollower: React.FC<MouseFollowerProps> = ({ children , text, className}) => {
   const [isHovering, setIsHovering] = useState(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
@@ -27,27 +31,47 @@ const MouseFollower: React.FC<MouseFollowerProps> = ({ children }) => {
 
   return (
     <div
-      className=" w-full h-full "
+      className=" w-full h-full cursor-none"
       onMouseMove={handleMouseMove}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-
+      
       {isHovering && (
-        <motion.img
-          src="/Hand.svg"
-          alt="Mouse Follower"
-          className="absolute w-16 h-16 pointer-events-none"
-          initial={{ opacity: 0 }}
+        <motion.div
+          className="absolute w-16 h-16 pointer-events-none z-50"
+          initial={{ opacity: 0}}
           animate={{ opacity: 1, x: position.x - 32, y: position.y - 32 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0, ease:"easeInOut" }}
-        />
+        >
+
+          <motion.div 
+          initial={{ opacity: 0, scale:1 }}
+          animate={{ opacity: 1 , scale:1.3, }}
+          transition={{ duration: 0.6 }}
+          className={cn("w-20 h-20 -z-40 flex items-center justify-center rounded-full", className)}>
+            <p className='text-center z-30'>
+              {text}
+            </p>
+          </motion.div>
+
+        </motion.div>
       )}
 
-{children}
+    {children}
     </div>
   );
 };
 
 export default MouseFollower;
+
+          {/* <Image           
+          src="/Ellipse.svg"
+          alt="Mouse Follower"
+          width={150}
+          height={150}
+          loading="eager"
+          className='relative w-32 h-32 -z-40'
+
+          /> */}
--- src/components/portfolio-item.tsx
+++ src/components/portfolio-item.tsx
@@ -4,56 +4,67 @@ import Link from "next/link"
 import Image from "next/image"
 import { portfolio_categories } from "@/lib/utils"
 import { motion, useAnimation } from "framer-motion"
+import MouseFollower from "./MouseFollower"
 
 
 export const PortfolioItem = (project: Project) => {
   const controls = useAnimation();
   return (
-    <motion.div 
-    whileHover={{ scale: 1.01 }}
-    transition={{ duration: 0.3 }}
-    className="relative group overflow-hidden rounded-lg border object-cover" style={{backgroundImage: `url(${project.yoast_head_json.og_image[0].url})`}}
-    onHoverStart={() => {
-      // Start animation on the animated div when hovered
-      controls.start({
-        opacity: 1,
-        y: 0,
-        transition: { duration: 0.25 },
-      });
-    }}
-    onHoverEnd={() => {
-      // Reset the animation when hover ends
-      controls.start({
-        opacity: 0,
-        y: 200,
-        transition: { duration: 0.25 },
-      });
-    }}
+    <motion.div
+      whileHover={{ scale: 1.01 }}
+      transition={{ duration: 0.3 }}
+      className="relative group overflow-hidden rounded-lg border object-cover" style={{ backgroundImage: `url(${project.yoast_head_json.og_image[0].url})` }}
+      onHoverStart={() => {
+        // Start animation on the animated div when hovered
+        controls.start({
+          opacity: 1,
+          y: 0,
+          transition: { duration: 0.25 },
+        });
+      }}
+      onHoverEnd={() => {
+        // Reset the animation when hover ends
+        controls.start({
+          opacity: 0,
+          y: 200,
+          transition: { duration: 0.25 },
+        });
+      }}
     >
       <Link href={`portfolio/${project.slug}`} prefetch={false}>
-      <motion.div 
-
+      <MouseFollower text="View Work" className="bg-brand/90">
+        <motion.div
           animate={controls}
-      className="w-full h-96 z-20 flex flex-col justify-center p-4 overflow-hidden bg-black/[.8]">
-        <motion.h3 
-          className="text-2xl font-semibold">
-            {project.title.rendered}
-          </motion.h3>
-        <motion.p         
-       className="text-muted-foreground text-3xl  overflow-auto">
-        {project.yoast_head_json.og_description}
-          {/* {project.portfolio_category.map(category => portfolio_categories[category]).join(', ')} */}
-        </motion.p>
-      </motion.div>
+          className="w-full h-[32rem] z-20 flex flex-col justify-evenly p-6 overflow-hidden bg-gray-800/40">
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xl font-semibold flex gap-1">
+                <div className="bg-brand w-3 h-3 rounded-full top-2 relative"/>
+                {project.title.rendered}
+              </h3>
+              <p className="text-3xl text-gray-300"   style={{
+                display: '-webkit-box',
+                WebkitLineClamp: 3, // Change this number to set how many lines you want to show
+                WebkitBoxOrient: 'vertical',
+                overflow: 'hidden',
+              }}>
+                {project.yoast_head_json.og_description}
+              </p>
+            </div>
+
+          <div className="flex gap-3">
+            {project.portfolio_category.map((category,index) => ( <span className="bg-brand/75 rounded-full px-2 py-1" key={index}> {portfolio_categories[category]} </span>))}
+          </div>
+        </motion.div>
+        </MouseFollower>
       </Link>
     </motion.div>
 
-  
+
   )
 }
 
-
-      {/* <Image
+// {project.portfolio_category.map(category => portfolio_categories[category]).join(', ')}
+{/* <Image
         src={
           project.yoast_head_json.og_image[0].url
         }
@@ -66,22 +77,22 @@ export const PortfolioItem = (project: Project) => {
         loading="eager"
       /> */}
 //   <motion.div
-  //   className="relative w-64 h-40 bg-blue-500 rounded-lg shadow-lg overflow-hidden"
-  //   whileHover={{ scale: 1.05 }}
-  // >
-  //   <motion.div
-  //     className="absolute inset-0 flex items-center justify-center bg-blue-700 bg-opacity-0 transition-all duration-300 hover:bg-opacity-70"
-  //   >
-  //     {/* Hidden text that appears on hover */}
-  //     <motion.p
-  //       className="text-white text-lg"
-  //       initial={{ opacity: 0 }}
-  //       animate={{ opacity: 0 }}
-  //       whileHover={{ opacity: 1 }}
-  //       transition={{ duration: 0.3 }}
-  //     >
-  //       Hovered Text
-  //     </motion.p>
-  //   </motion.div>
+//   className="relative w-64 h-40 bg-blue-500 rounded-lg shadow-lg overflow-hidden"
+//   whileHover={{ scale: 1.05 }}
+// >
+//   <motion.div
+//     className="absolute inset-0 flex items-center justify-center bg-blue-700 bg-opacity-0 transition-all duration-300 hover:bg-opacity-70"
+//   >
+//     {/* Hidden text that appears on hover */}
+//     <motion.p
+//       className="text-white text-muted-foreground text-lg"
+//       initial={{ opacity: 0 }}
+//       animate={{ opacity: 0 }}
+//       whileHover={{ opacity: 1 }}
+//       transition={{ duration: 0.3 }}
+//     >
+//       Hovered Text
+//     </motion.p>
+//   </motion.div>
 
-  // </motion.div>
+// </motion.div>
